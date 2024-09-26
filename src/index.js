2932cc47bcc30be5848ae81abac8045003f1e1c4
anbuihoang <an.buihoang@ncc.asia>
2024-09-26 11:39:23 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import morgan from 'morgan';
+import route from './routes/index.js';
 
 const app = express()
 const port = 3000
 
 app.use(express.static('src/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Routes init
+route(app)
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -14,14 +20,6 @@ app.use(morgan('combined'))
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', 'src/resources/views');
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import newsRoute from './news.js';
+import siteRoute from './site.js';
+
+function route(app) {
+  app.use('/news', newsRoute)
+  app.use('/', siteRoute)
+}
+
+export default route
