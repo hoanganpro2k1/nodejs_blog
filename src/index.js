356cf59eea8608ab7eb69fee95845a8a91900286
anbuihoang <an.buihoang@ncc.asia>
2024-09-26 15:44:32 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ route(app);
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', engine({ extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.set('views', 'src/resources/views')
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'src/resources/views');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
